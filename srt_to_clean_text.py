442fdd5e50ef492f77f44ef8cf12f87fbf7c93ed
Sarah Mak <59949361+ThePlasmak@users.noreply.github.com>
2024-09-25 23:06:12 +0800

    Remove example

--- srt_to_clean_text.py
+++ srt_to_clean_text.py
@@ -1,40 +1,7 @@
 import pyperclip
 
 # Input text
-input_text = """So right after 11pm, I brought him to sleep, about 12pm, I think I heard some sound, so I realised, eh, what got smell?
-
-10
-00:02:10,940 --> 00:02:11,660
-Fiona: Then I woke up,
-
-11
-00:02:11,660 --> 00:02:13,280
-Fiona: oh he actually had a diarrhoea,
-
-12
-00:02:13,280 --> 00:02:13,520
-Fiona: a very
-
-13
-00:02:13,520 --> 00:02:14,520
-Fiona: bad diarrhoea,
-
-14
-00:02:14,520 --> 00:02:16,860
-Fiona: so I wanted to clean him up and
-
-15
-00:02:16,860 --> 00:02:17,960
-Fiona: I realised that his
-
-16
-00:02:17,960 --> 00:02:18,440
-Fiona: whole
-
-17
-00:02:18,440 --> 00:02:40,500
-Fiona: body was very jelly and I actually screamed for my parents. and when I went to scream for my parents, he actually made a very loud sound, he made a girl's voice screaming like, ah, very loud. So I went over to him and he actually slowly gaps in for air and so he passed on when everyone is at home.
-
+input_text = """
 """
 
 
